58205e986211a3031705710201b501f1b4a2f185
castro12321 <norbertkawinski@gmail.com>
2014-06-26 18:57:40 +0200

    Added TerrainTypes enum

---
+++ Plevian/TerrainTypes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plevian
+{
+    public enum TerrainTypes
+    {
+        VILLAGE = 1,
+        PLAINS,
+        MOUNTAINS,
+        LAKES
+    };
+}
