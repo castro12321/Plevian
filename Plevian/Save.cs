15efba64ad1ec6a892bcb21cf86e6f4bc5f64ce6
castro12321 <norbertkawinski@gmail.com>
2014-06-26 20:02:17 +0200

    Added save class

---
+++ Plevian/Save.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plevian
+{
+    class Save
+    {
+        private readonly String path;
+
+        public Save(String path)
+        {
+            this.path = path + "\\";
+        }
+
+        public String getMapFile()
+        {
+            return path + "map.txt";
+        }
+    }
+}
