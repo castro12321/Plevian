102415780c6bdb6024f6d2ae2b55c9d94e973a56
Damian <lubiebardzozelki@gmail.com>
2014-06-26 20:46:27 +0200

    Resources handling

--- Plevian/Resources.cs
+++ Plevian/Resources.cs
@@ -8,5 +8,62 @@ namespace Plevian
 {
     class Resources
     {
+        int food,
+            wood,
+            iron,
+            stone;
+        
+        public Resources(int food, int wood, int iron, int stone)
+        {
+            this.food = food;
+            this.wood = wood;
+            this.iron = iron;
+            this.stone = stone;
+        }
+
+        public Resources()
+        {
+            wood = food = iron = stone = 0;
+        }
+
+        public static Resources operator +(Resources lh, Resources rh)
+        {
+            Resources res = new Resources();
+            res.wood = lh.wood + rh.wood;
+            res.iron = lh.iron + rh.iron;
+            res.stone = lh.stone + rh.stone;
+            res.food = lh.food + rh.food;
+            return res;
+        }
+
+        public static Resources operator -(Resources lh, Resources rh)
+        {
+            Resources res = new Resources();
+            res.wood = lh.wood - rh.wood;
+            res.iron = lh.iron - rh.iron;
+            res.stone = lh.stone - rh.stone;
+            res.food = lh.food - rh.food;
+            return res;
+        }
+
+        public static bool operator >(Resources lh, Resources rh)
+        {
+            return (lh.food > rh.food && lh.stone > rh.stone && lh.wood > rh.wood && lh.iron > rh.iron);
+        }
+
+        public static bool operator >=(Resources lh, Resources rh)
+        {
+            return (lh.food >= rh.food && lh.stone >= rh.stone && lh.wood >= rh.wood && lh.iron >= rh.iron);
+        }
+
+        public static bool operator <(Resources lh, Resources rh)
+        {
+            return (lh.food < rh.food || lh.stone < rh.stone || lh.wood < rh.wood || lh.iron < rh.iron);
+        }
+
+        public static bool operator <=(Resources lh, Resources rh)
+        {
+            return (lh.food <= rh.food || lh.stone <= rh.stone || lh.wood <= rh.wood || lh.iron <= rh.iron);
+        }
     }
 }
