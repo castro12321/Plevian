dafbfe4b2d846074bffa4ce60d0e57ea869e63ee
Damian <lubiebardzozelki@gmail.com>
2014-06-26 19:20:27 +0200

    skeleton of the Map

--- Plevian/Map.cs
+++ Plevian/Map.cs
@@ -8,6 +8,28 @@ namespace Plevian
 {
     class Map
     {
+        private byte[,] fields;
+
+        public Map(int x, int y)
+        {
+            fields = new byte[x, y];
+            generateMap();
+        }
+
+        /// <summary>
+        /// Loads map from file
+        /// </summary>
+        /// <param name="path">path to the file</param>
+        public Map(string path)
+        {
+
+        }
+
+        private void generateMap()
+        {
+        }
+
+        public void saveMap();
 
     }
 }
