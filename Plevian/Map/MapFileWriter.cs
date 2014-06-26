147be77d78a335a32a28b688325300f9177bf86f
castro12321 <norbertkawinski@gmail.com>
2014-06-26 19:25:41 +0200

    Moved map related classes to Map directory

---
+++ Plevian/Map/MapFileWriter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Plevian
+{
+    class MapFileWriter
+    {
+        public Map read(File mapFile)
+        {
+            return null;
+        }
+    }
+}
