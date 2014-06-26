135cd3a286a9adb2fb8674cf7f47c03d1f272c61
castro12321 <norbertkawinski@gmail.com>
2014-06-26 19:38:29 +0200

    Added Location class

---
+++ Plevian/Map/Location.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plevian.Map
+{
+    class Location
+    {
+        public readonly int x, y;
+
+        public Location(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
