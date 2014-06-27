3c19acf8d078f33dbe4d16493df5f9f659957849
castro12321 <norbertkawinski@gmail.com>
2014-06-27 17:43:47 +0200

    Implemented basic LocalTime

--- Plevian/LocalTime.cs
+++ Plevian/LocalTime.cs
@@ -6,11 +6,26 @@ using System.Threading.Tasks;
 
 namespace Plevian
 {
-    class LocalTime
+    public class LocalTime
     {
         /// <summary>
         /// Represents virtual seconds since game start
         /// </summary>
-        private ulong seconds;
+        public ulong seconds { get; private set; }
+
+        public LocalTime(ulong hours, ulong minutes, ulong seconds)
+            : this(hours*60*60 + minutes*60 + seconds)
+        {
+        }
+
+        public LocalTime(ulong seconds)
+        {
+            this.seconds = seconds;
+        }
+
+        public static LocalTime operator +(LocalTime lh, LocalTime rh)
+        {
+            return new LocalTime(lh.seconds + rh.seconds);
+        }
     }
 }
