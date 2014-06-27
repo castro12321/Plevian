5cf158cba5ed39a13e28c6f5063e567fb67002dc
castro12321 <norbertkawinski@gmail.com>
2014-06-27 13:42:43 +0200

    Added BuildingType and UnitType enums

---
+++ Plevian/Buildings/BuildingType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plevian.Buildings
+{
+    public enum BuildingType
+    {
+        FARM = 1,
+        TOWN_HALL,
+        MINE
+    };
+}
