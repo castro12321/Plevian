b153b322355e82cf7ee9d72552f69475c3865fca
Damian <lubiebardzozelki@gmail.com>
2014-06-27 18:55:52 +0200

    Added some knight thingies.

--- Plevian/Units/Knigth.cs
+++ Plevian/Units/Knigth.cs
@@ -15,22 +15,28 @@ namespace Plevian.Units
         public static readonly int defenseOnCavalry = 5;
         public static readonly int defenseOnArchers = 10;
 
+        public static readonly int movementSpeed = 25;
+        public static readonly int lootCapacity = 50;
 
+        public static readonly Resources.Resources recruitCost = new Iron(100) + new Food(50) + new Wood(25);
+        public static readonly Resources.Resources upkeepCost = new Iron(1) + new Food(5);
+
+        public static readonly UnitType unitType = UnitType.KNIGHT;
 
 
         public int getAttackStrength() { return attackStrength; }
 
-        public virtual int getDefenseOnInfantry();
-        public virtual int getDefenseOnCavalry();
-        public virtual int getDefenseOnArchers();
+        public virtual int getDefenseOnInfantry() { return defenseOnInfantry; }
+        public virtual int getDefenseOnCavalry() { return defenseOnCavalry; }
+        public virtual int getDefenseOnArchers() { return defenseOnArchers; }
 
-        public virtual int GetMovementSpeed();
+        public virtual int GetMovementSpeed() { return movementSpeed; }
 
-        public virtual int getLootCapacity();
+        public virtual int getLootCapacity() { return lootCapacity; }
 
-        public virtual Resources.Resources getRecruitCost();
-        public virtual Resources.Resources getUpkeepCost();
+        public virtual Resources.Resources getRecruitCost() { return recruitCost; }
+        public virtual Resources.Resources getUpkeepCost() { return upkeepCost; }
 
-        public virtual UnitType getUnitType();
+        public virtual UnitType getUnitType() { return unitType; }
     }
 }
