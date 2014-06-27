77010aaa77fb6232b98075dc16933f81b4992ca9
castro12321 <norbertkawinski@gmail.com>
2014-06-27 16:39:09 +0200

    Added LocalTime to the project. Will be implemented later

---
+++ Plevian/LocalTime.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plevian
+{
+    class LocalTime
+    {
+        /// <summary>
+        /// Represents virtual seconds since game start
+        /// </summary>
+        private ulong seconds;
+    }
+}
