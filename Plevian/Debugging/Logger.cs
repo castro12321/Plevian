984f8ae79ea678c50caf5fec569df8ed44216f50
castro12321 <norbertkawinski@gmail.com>
2014-06-28 23:25:30 +0200

    Added my Logger

---
+++ Plevian/Debugging/Logger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plevian.Debugging
+{
+    class Logger
+    {
+        private static const bool c = true;
+        /// <summary>
+        /// Logger for castro
+        /// </summary>
+        public static void c(String msg)
+        {
+            if (c)
+                Console.WriteLine(msg);
+        }
+    }
+}
