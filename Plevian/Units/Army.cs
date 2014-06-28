a309efe071053a19872e4dafc7ec549b0be12843
Damian <lubiebardzozelki@gmail.com>
2014-06-28 12:51:22 +0200

    Army and Battle class added.

---
+++ Plevian/Units/Army.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plevian.Units
+{
+    class Army
+    {
+        private Dictionary<UnitType, Unit> units = new Dictionary<UnitType, Unit>();
+        
+        public Army()
+        {}
+
+        public static Army operator + ( Army lh, Army rh )
+        {
+            Army army = new Army();
+            foreach (var key in lh.units)
+            {
+               
+            }
+
+            return army;
+        }
+
+
+
+    }
+}
