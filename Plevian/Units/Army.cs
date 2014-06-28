153d604a9f531c0cad4216ca345ba51dc913211d
Damian <lubiebardzozelki@gmail.com>
2014-06-28 13:12:25 +0200

    Army, first operators added.

--- Plevian/Units/Army.cs
+++ Plevian/Units/Army.cs
@@ -16,12 +16,38 @@ namespace Plevian.Units
         public static Army operator + ( Army lh, Army rh )
         {
             Army army = new Army();
-            foreach (var key in lh.units)
+            foreach (var pair in lh.units)
             {
-               
+                army.units.Add(pair.Key, pair.Value);
             }
 
+            foreach (var pair in rh.units)
+            {
+                if (army.units[pair.Key] != null)
+                {
+                    army.units[pair.Key].quanity += pair.Value.quanity;
+                }
+                else
+                {
+                    army.units.Add(pair.Key, pair.Value);
+                }
+            }
             return army;
+        }
+
+        public static Army operator -(Army lh, Army rh)
+        {
+            return new Army();
+        }
+
+        public static bool operator >=(Army lh, Army rh)
+        {
+            foreach (var pair in lh.units)
+            {
+                if (rh.units[pair.Key] == null) return false;
+                if (rh.units[pair.Key].quanity > pair.Value.quanity) return false;
+            }
+            return true;
         }
 
 
