2b01437792fe17ca1c42d53c5913a229e5237eb4
castro12321 <norbertkawinski@gmail.com>
2014-06-29 14:43:18 +0200

    Adjusted code to the changes

--- Plevian/Game.cs
+++ Plevian/Game.cs
@@ -13,7 +13,7 @@ namespace Plevian
 
         public Game()
         {
-            gameTime = new GameTime(new LocalTime(0));
+            GameTime.init(0);
         }
 
         public Game(Save save)
@@ -26,7 +26,7 @@ namespace Plevian
         public void tick()
         {
             System.Threading.Thread.Sleep(250);
-            ulong timediff = gameTime.updateTime();
+            ulong timediff = GameTime.update();
             while (timediff --> 0)
             {
                 village.tick();
