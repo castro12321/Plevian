0e04ab78a2072635abd6a58ed09e3dec6c6052bc
Damian <lubiebardzozelki@gmail.com>
2014-07-03 01:54:27 +0200

    little changes in battle to make it work. (It's not working on develop, damn!)

---
+++ Tests/Battles/testBattles.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Plevian.Battles;
+using Plevian.Units;
+namespace Tests.Battles
+{
+    [TestClass]
+    public class testBattles
+    {
+        [TestMethod]
+        public void tastBattleSystem()
+        {
+            Army attacker = new Army(), defender = new Army();
+
+            attacker += new Knight(100);
+            attacker += new Archer(5);
+
+            defender += new Warrior(25);
+
+            Battle battle = new Battle(attacker, defender, 0f, 0f);
+            Report report = battle.makeBattle();
+
+            Assert.IsTrue(report.battleResult == BattleState.AttackerVictory);
+            Assert.IsTrue(report.attackerArmy[UnitType.KNIGHT] == 100);
+            Assert.IsTrue(report.attackerArmy[UnitType.ARCHER] == 5);
+            Assert.IsTrue(report.defenderArmy[UnitType.WARRIOR] == 25);
+            Assert.IsTrue(report.defenderLosses[UnitType.WARRIOR] == 25);
+
+            defender += new Knight(300);
+
+            battle = new Battle(attacker, defender, 0f, 0f);
+            report = battle.makeBattle();
+
+            Assert.IsTrue(report.defenderArmy[UnitType.KNIGHT] == 300);
+            Assert.IsTrue(report.battleResult == BattleState.DefenderVictory);
+        }
+
+
+    }
+}
