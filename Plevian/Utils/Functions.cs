5e6aa0a5a9e47bb3cecb78c40b0fab882b7ad240
Damian <lubiebardzozelki@gmail.com>
2014-07-03 13:10:04 +0200

    added collision cheking function

---
+++ Plevian/Utils/Functions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plevian.Utils
+{
+    public class Functions
+    {
+        public static bool Collision(int x1, int x2, int y1, int y2, int w1, int w2, int h1, int h2)
+        {
+            /*
+             * x,y           w
+             *   ----------------------
+             *   -
+             *  h-
+             *   -
+             *   ----------------------*/
+            if (x1 + w1 >= x2 && 
+                x1 <= x2 + w2 && 
+                y1 + h1 >= y2 && 
+                y1 <= y2 + h2)
+                return true;
+
+
+            return false;
+        }
+
+    }
+}
