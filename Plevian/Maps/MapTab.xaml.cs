4eb39182fe6f77ea61bf0693b12979ec3d4f5946
castro12321 <norbertkawinski@gmail.com>
2014-07-20 14:21:24 +0200

    Separated tabs to own custom user controls

---
+++ Plevian/Maps/MapTab.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Plevian.Debugging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Plevian.Maps
+{
+    /// <summary>
+    /// Interaction logic for MapTab.xaml
+    /// </summary>
+    public partial class MapTab : UserControl
+    {
+        private MapView mapView;
+
+        public MapTab(Map map)
+        {
+            InitializeComponent();
+
+            mapView = new MapView(map);
+            sfml_map.Child = mapView;
+
+            mapView.PlevianMouseMovedEvent += mapView_PlevianMouseMovedEvent;
+            mapView.PlevianTileClickedEvent += mapView_PlevianTileClickedEvent;
+        }
+        
+        public void handleEvents()
+        {
+            mapView.handleEvents();
+        }
+
+        public void render()
+        {
+            mapView.render();
+        }
+
+        void mapView_PlevianTileClickedEvent(object sender, TileClickedEventArgs e)
+        {
+            Logger.c("event click at " + e.clicked + " " + e.clickedTileLocation.x + " " + e.clickedTileLocation.y);
+        }
+
+        void mapView_PlevianMouseMovedEvent(object sender, MouseMovedEventArgs e)
+        {
+            Plevian.Debugging.Logger.c("event move to " + e.mouseLocation.x + " " + e.mouseLocation.y);
+        }
+    }
+}
---
+++ Plevian/Villages/VillageTab.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Plevian.Maps;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Plevian.Villages
+{
+    /// <summary>
+    /// Interaction logic for VillageTab.xaml
+    /// </summary>
+    public partial class VillageTab : UserControl
+    {
+        private VillageView villageView;
+
+        public VillageTab(Game game)
+        {
+            InitializeComponent();
+
+            villageView = new VillageView(game);
+            sfml_village.Child = villageView;
+        }
+
+        public void handleEvents()
+        {
+            villageView.handleEvents();
+        }
+
+        public void render()
+        {
+            villageView.render();
+        }
+    }
+}
