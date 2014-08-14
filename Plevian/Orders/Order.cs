02b45bb232589efef2633dbd059a352b93304225
Damian <lubiebardzozelki@gmail.com>
2014-08-14 18:52:08 +0200

    some kind of commit.

--- Plevian/Orders/Order.cs
+++ Plevian/Orders/Order.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Plevian.Maps;
+using Plevian.Units;
 namespace Plevian.Orders
 {
     public class Order
@@ -12,11 +13,15 @@ namespace Plevian.Orders
         Location destination;
         Seconds duration;
         GameTime endTime;
+        Army army;
 
-        public Order(Location origin, Location destination, float timePerTile)
+
+
+        public Order(Location origin, Location destination, float timePerTile, Army army)
         {
             this.origin = origin;
             this.destination = destination;
+            this.army = army;
 
             float distance = origin.distance(destination);
             duration = new Seconds((int) (timePerTile * distance));
@@ -25,7 +30,7 @@ namespace Plevian.Orders
         }
 
 
-        public abstract onEnd(
+        public abstract onEnd();
 
 
     }
