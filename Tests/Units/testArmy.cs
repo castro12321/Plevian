9c23f2e51eabad84606c4ae8d123d1818f835ac3
castro12321 <norbertkawinski@gmail.com>
2014-08-23 19:30:20 +0200

    Fixed loot counting in tests

--- Tests/Units/testArmy.cs
+++ Tests/Units/testArmy.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Plevian.Units;
+using Plevian.Debugging;
 
 namespace Tests.Units
 {
@@ -13,7 +14,7 @@ namespace Tests.Units
             Army army = new Army();
             Unit knight = new Knight(100);
             Unit archer = new Archer(200);
-            int capacity = knight.getLootCapacity() + archer.getLootCapacity();
+            int capacity = (knight.getLootCapacity() * 100) + (archer.getLootCapacity() * 200);
             army += knight;
             army += archer;
             Assert.IsTrue(capacity == army.getLootCapacity());
