6638bcc9a8fb18ec497de12ab952d23c0ddfd10b
castro12321 <kawa12321@gmail.com>
2014-09-18 10:44:58 +0200

    Implemented more resource-wise AI grow

--- Plevian/Players/ComputerPlayer.cs
+++ Plevian/Players/ComputerPlayer.cs
@@ -50,33 +50,56 @@ namespace Plevian.Players
         {
             Logger.AI(name + " village tick:");
 
+            // Most important - Buildings
             if(village.buildingsQueue.Count == 0)
             {
                 BuildingType toBuildType = RandomBuildingType();
                 Logger.AI("Trying build " + toBuildType);
                 if(village.canBuild(toBuildType))
                 {
-                    Logger.AI("Building " + toBuildType);
-                    village.build(toBuildType);
+                    switch(toBuildType)
+                    {
+                        // Allow buildings that produce resources
+                        case BuildingType.MINE:
+                        case BuildingType.FARM:
+                        case BuildingType.LUMBER_MILL:
+                            Logger.AI("Building res " + toBuildType);
+                            village.build(toBuildType);
+                            break;
+                        // For other buildings make sure that you have at least 5x more resources than needed
+                        default:
+                        {
+                            if (village.resources.canAfford(village.getPriceForNextLevel(toBuildType) * 5))
+                            {
+                                Logger.AI("Building " + toBuildType);
+                                village.build(toBuildType);
+                            }
+                            break;
+                        }
+                    }
                 }
             }
 
+            // Second most important - Army
             if(village.recruitQueue.Count == 0)
             {
                 Unit toRecruit = RandomUnit(village);
                 Logger.AI("Trying recruit " + toRecruit);
-                if(village.canRecruit(toRecruit))
+                if(village.canRecruit(toRecruit)
+                && village.resources.canAfford(village.resources * 15))
                 {
                     Logger.AI("Recruiting " + toRecruit);
                     village.recruit(toRecruit);
                 }
             }
 
+            // Least important - Technology
             if(village.researchQueue.Count == 0)
             {
                 Technology toResearch = RandomTechnology(village);
                 Logger.AI("Trying research " + toResearch);
-                if(village.canResearch(toResearch))
+                if(village.canResearch(toResearch)
+                && village.resources.canAfford(toResearch.Price * 5))
                 {
                     Logger.AI("Researching " + toResearch);
                     village.research(toResearch);
